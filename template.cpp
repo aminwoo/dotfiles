331c8a84acad62d9006272e5a8d914c99a9932d2
Ben <bwoo2718@gmail.com>
2024-10-01 21:10:40 +1000

    add c++ template

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+void solve() {
+    int n;
+    cin >> n;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    while (T--)
+        solve();
+}
