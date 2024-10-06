3511cd6bcb2ab775a8169885f790d52c723cbad0
Ben <bwoo2718@gmail.com>
2024-10-06 18:24:09 +1100

    Fix formatting

--- template.cpp
+++ template.cpp
@@ -4,18 +4,20 @@ using namespace std;
 
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-int n, k;
+int n, m, k, q;
 const int N = 500005;
 long long a[N];
 vector<int> adj[N];
+string s;
 
-void solve() {}
+void solve() {
+}
 
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  int T;
-  cin >> T;
-  while (T--)
-    solve();
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    while (T--)
+        solve();
 }
