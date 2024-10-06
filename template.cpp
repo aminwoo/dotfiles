1c2f091f78a68c542fd3a7a08a9417a816101454
Ben <bwoo2718@gmail.com>
2024-10-06 16:20:07 +1100

    Use long long instead of ll

--- template.cpp
+++ template.cpp
@@ -2,12 +2,11 @@
 
 using namespace std;
 
-typedef long long ll;
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 int n, k;
 const int N = 500005;
-ll a[N];
+long long a[N];
 vector<int> adj[N];
 
 void solve() {}
