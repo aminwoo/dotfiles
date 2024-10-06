cc7afbd563d0f217f2476f915bef8f0dc11eb219
Ben <bwoo2718@gmail.com>
2024-10-06 21:27:28 +1100

    Add y_combinator template

--- template.cpp
+++ template.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+template <class Fun> class y_combinator_result {
+    Fun fun_;
+
+  public:
+    template <class T>
+    explicit y_combinator_result(T &&fun) : fun_(std::forward<T>(fun)) {
+    }
+    template <class... Args> decltype(auto) operator()(Args &&...args) {
+        return fun_(std::ref(*this), std::forward<Args>(args)...);
+    }
+};
+template <class Fun> decltype(auto) y_combinator(Fun &&fun) {
+    return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+}
+#define yc y_combinator
+
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 int n, m, k, q;
