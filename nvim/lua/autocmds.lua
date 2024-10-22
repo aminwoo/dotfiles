3ca649d80643e607d7576085175148c8749ec102
Ben <bwoo2718@gmail.com>
2024-10-22 17:12:21 +1100

    Update NvChad config

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,4 +1,17 @@
 vim.api.nvim_create_autocmd("FileType", {
+	pattern = "go",
+	callback = function()
+		vim.api.nvim_buf_set_keymap(
+			0,
+			"n",
+			"<C-c>",
+			":w<bar>:split<CR>:te go run % <CR>i",
+			{ noremap = true, silent = true }
+		)
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
 	pattern = "rust",
 	callback = function()
 		vim.api.nvim_buf_set_keymap(
@@ -33,6 +46,19 @@ vim.api.nvim_create_autocmd("FileType", {
 			"n",
 			"<C-c>",
 			":w<bar>:split<CR>:te node %<CR>i",
+			{ noremap = true, silent = true }
+		)
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "typescript",
+	callback = function()
+		vim.api.nvim_buf_set_keymap(
+			0,
+			"n",
+			"<C-c>",
+			":w<bar>:split<CR>:te npx tsx %<CR>i",
 			{ noremap = true, silent = true }
 		)
 	end,
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,5 +1,11 @@
 return {
-
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opts = function()
+      return require "configs.null-ls"
+    end,
+  },
 	{
 		"nvim-cmp",
 		dependencies = { "hrsh7th/cmp-emoji" },
